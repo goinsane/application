c68e19950c4a3c51100819f9cc51111cf458bfbe
Orkun Karaduman <orkunkaraduman@gmail.com>
2020-06-18 15:40:50 +0300

    added Name()

--- version.go
+++ version.go
@@ -1,9 +1,15 @@
 package application
 
 var (
+	name    string
 	version string
 	build   string
 )
+
+// Name returns application.name defined with ldflags.
+func Name() string {
+	return name
+}
 
 // Version returns application.version defined with ldflags.
 func Version() string {
